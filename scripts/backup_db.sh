52fd8be37a3ea798318b3439185f063caf11f76b
baseluzmani <baseluzmani@gmail.com>
2026-07-24 19:33:15 +0100

    ETF section: Changes, Compare, Fund compare; universe loader; per-tab intros

--- scripts/backup_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DB=/home/ahmet/FTScrapper/data/funds.db
-OUT=/home/ahmet/backups/funds_$(date +%Y%m%d_%H%M).db
-sqlite3 "$DB" ".backup '$OUT'"
-gzip -f "$OUT"
-find /home/ahmet/backups -name 'funds_*.db.gz' -mtime +21 -delete
